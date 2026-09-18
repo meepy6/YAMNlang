4169b7af46a2ef82ea520a8228a06bc74e4818f9
meepy6 <alastairbarras@icloud.com>
2026-09-18 14:01:54 +0100

    Added Trie tree class and header, trie-tree.cpp/hpp

---
+++ trie-tree.hpp
@@ -0,0 +1,25 @@
+#ifndef TRIE_H
+#define TRIE_H
+
+#include <unordered_map>
+#include <memory>
+#include <string>
+
+struct TrieNode {
+    std::unordered_map<char, std::unique_ptr<TrieNode>> children;
+    bool isEndOfPath = false;
+};
+
+class Trie {
+private:
+    std::unique_ptr<TrieNode> root;
+
+public:
+    Trie();
+
+    void insert(const std::string& word);
+    bool search(const std::string& word) const;
+    bool startsWith(const std::string& prefix) const;
+};
+
+#endif
