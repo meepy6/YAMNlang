eb8de10857e2ee30dd87f9f93b90802fbd0b83cd
meepy6 <alastairbarras@icloud.com>
2026-09-22 07:49:12 +0100

    Added trie-class folder containing implementation of a "Prefix Tree" class, made by me. Not used yet, but is planned to be because I am rewriting my loop to be less incredibly inefficient.

---
+++ trie-class/trie-tree.hpp
@@ -0,0 +1,28 @@
+#ifndef TRIE_H
+#define TRIE_H
+
+#include <unordered_map>
+#include <memory>
+#include <string>
+
+namespace trie {
+    struct TrieNode {
+        std::unordered_map<char, std::unique_ptr<TrieNode>> children;
+        bool is_end_of_path = false;
+    };
+
+    class Trie {
+    private:
+        std::unique_ptr<TrieNode> root;
+
+    public:
+        Trie();
+
+        void insert(const std::string& word);
+        bool search(const std::string& word) const;
+        bool startsWith(const std::string& prefix) const;
+    };
+}
+
+
+#endif
